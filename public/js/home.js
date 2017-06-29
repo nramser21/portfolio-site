420062959efb11cf76f7872f8481ad1bce5d5fa3
Nick <nramser21@gmail.com>
2017-06-29 01:02:19 -0400

    Finished styling basic carousel

--- public/js/home.js
+++ public/js/home.js
@@ -13,21 +13,26 @@ function showSlide(n) {
 $(document).ready(() => {
   var index = 0;
   var slides = $(".slides").children();
-  var dots = $(".dots");
+  var dots = $(".dots").children();
 
   for (i = index + 1; i < slides.length; i++) {
     slides.eq(i).hide();
+    dots.eq(i).css("background-color", "rgb(130, 130, 130)");
   }
 
   $(".prev").click(() => {
     slides.eq(index).hide();
-    index = index > 0 ? --index : slides.length;
+    dots.eq(index).css("background-color", "rgb(130, 130, 130)");
+    index = index > 0 ? --index : slides.length - 1;
     slides.eq(index).fadeIn("slow");
+    dots.eq(index).css("background-color", "#A6400A");
   });
   $(".next").click(() => {
     slides.eq(index).hide();
+    dots.eq(index).css("background-color", "rgb(130, 130, 130)");
     index = index < slides.length - 1 ? ++index : 0;
     slides.eq(index).fadeIn("slow");
+    dots.eq(index).css("background-color", "#A6400A");
   });
 
 })
