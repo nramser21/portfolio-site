325ece0ebc2e9f9cb0d1d18924bb12dc7197d72c
Nick <nramser21@gmail.com>
2017-07-03 19:14:29 -0400

    Added routes for project pages

--- app/routes.js
+++ app/routes.js
@@ -14,7 +14,43 @@ router.get('/blog', (req, res) => {
 });
 
 router.get('/projects', (req, res) => {
-  res.render('pages/projects');
+  res.render('pages/projects/projects');
+});
+
+router.get('/projects/fcc', (req, res) => {
+  res.render('pages/projects/fcc');
+});
+
+router.get('/projects/shell', (req, res) => {
+  res.render('pages/projects/shell');
+});
+
+router.get('/projects/portfolio', (req, res) => {
+  res.render('pages/projects/portfolio');
+});
+
+router.get('/projects/school', (req, res) => {
+  res.render('pages/projects/school');
+});
+
+router.get('/projects/legacy', (req, res) => {
+  res.render('pages/projects/legacy');
+});
+
+router.get('/projects/iss-tracker', (req, res) => {
+  res.render('pages/projects/iss-tracker');
+});
+
+router.get('/projects/freelance', (req, res) => {
+  res.render('pages/projects/freelance');
+});
+
+router.get('/projects/learning', (req, res) => {
+  res.render('pages/projects/learning');
+});
+
+router.get('/projects/podcast', (req, res) => {
+  res.render('pages/projects/podcast');
 });
 
 router.get('/contact', (req, res) => {
